8090652f8b27a64b4a1929b6dfc9df5e402d7d6e
Mao Li <anrenyil@gmail.com>
2019-09-07 16:27:44 -0700

    fix bug

--- blog_on_rails/app/controllers/comments_controller.rb
+++ blog_on_rails/app/controllers/comments_controller.rb
@@ -4,14 +4,14 @@ class CommentsController < ApplicationController
         @comment = Comment.new comment_params
         @comment.post = @post
         if @comment.save
-            redirect_to post_path(@post)
+            # redirect_to post_path(@post)
         else
         @comments = @post.comments.order(created_at: :desc)
         render 'posts/show'
         end    
     end
     def destroy
-        @post = Post.find(params[:product_id])
+        @post = Post.find(params[:post_id])
         @comment = Comment.find(params[:id])
         @comment.destroy
         redirect_to post_path(@post), notice: 'Comment Deleted'
